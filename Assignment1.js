dc537a552c9ec94e02d2febb69868bc833ca2e7c
Suranjana30 <90706866+Suranjana30@users.noreply.github.com>
2022-01-26 14:02:29 +0530

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,14 @@
+//1.Learn About JS Runtime
+console.log("HELLO ! WELCOME TO JAVASCRIPT");
+const a=100;
+const b=5;
+const result=a*b;
+console.log("result:"+result)
+
+//2.calculate the area of the circle using variable and operators.
+let radius;
+let area;
+radius=5;
+area=(3.14*radius*radius);
+console.log("The radius of the circle is:"+radius);
+console.log("The area of the circle is:"+area);
